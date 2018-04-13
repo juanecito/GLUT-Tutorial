f0cf0107195d681fc5084a58f154e82bb3166eb3
juan <juan@aixplorer>
2018-04-13 18:19:21 +0200

    modication example 2

--- src/example_2.cpp
+++ src/example_2.cpp
@@ -195,20 +195,28 @@ void initializeGL(GLuint& program)
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s),
+    // and then configure vertex attributes(s).
     glBindVertexArray(VAO);
+    {
+    	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    	{
+    		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    		glEnableVertexAttribArray(0);
+    	}
+    	// note that this is allowed, the call to glVertexAttribPointer
+    	// registered VBO as the vertex attribute's bound vertex buffer object
+    	// so afterwards we can safely unbind
+    	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    }
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally
+    // modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't
+    // unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0);
 
 }
